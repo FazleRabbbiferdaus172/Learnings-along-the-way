007b26f80e9cecc0e3162b0c5d1bbd06054d5278
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2021-01-26 20:18:20 +0600

    Add files via upload

---
+++ react/Import_React_new.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+    render() {
+        return <h1>Hello world</h1>;
+    }
+};
+
+ReactDOM.render(<MyComponentClass />, document.getElementById('app'));
