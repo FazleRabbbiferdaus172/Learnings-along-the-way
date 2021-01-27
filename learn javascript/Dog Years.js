872cbafead37eb9f15b21ed1782c381da24614b3
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2021-01-28 00:51:05 +0600

    Add files via upload

---
+++ learn javascript/Dog Years.js
@@ -0,0 +1,9 @@
+let myAge = 51;
+let earlyYears = 2;
+earlyYears *= 10.5;
+myAge -= 2;
+let laterYears = myAge;
+laterYears *= 4;
+let myAgeInDogYears = myAge + laterYears;
+const myName = "Aka".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
