dbf03d73f7f1aeae6c850f3d874d7c9c70c0be37
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2021-01-29 04:15:30 +0600

    Add files via upload

---
+++ Authorization Form.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    let x = "";
+    if (this.state.authorized)
+    {
+      x = "Contact";
+    }
+    else{
+      x = "Enter the Password";
+    }
+
+    let login = (
+      <form action="#" onSubmit = {this.authorize}>
+        <input type="password" placeholder="Password"/>
+        <input type="submit"/>
+      </form>
+      );
+      let contactInfo = (
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+      );
+    return (
+      <div id="authorization">
+        <h1>{x}</h1>
+        {this.state.authorized ? contactInfo: login}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Contact />, 
+  document.getElementById('app')
+);
