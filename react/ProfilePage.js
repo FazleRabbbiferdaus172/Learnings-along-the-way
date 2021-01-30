2ddc9ffb0b9708edd9556bbdc37b1ec6d0337bb6
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2021-01-31 03:22:04 +0600

    Add files via upload

---
+++ react/ProfilePage.js
@@ -0,0 +1,14 @@
+class ProfilePage extends React.Component {
+    render() {
+      return (
+        <div>
+          <NavBar />
+          <h1>All About Me!</h1>
+          <p>I like movies and blah blah blah blah blah</p>
+          <img src="https://content.codecademy.com/courses/React/react_photo-monkeyselfie.jpg" />
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<ProfilePage/>, document.getElementById('app'));
