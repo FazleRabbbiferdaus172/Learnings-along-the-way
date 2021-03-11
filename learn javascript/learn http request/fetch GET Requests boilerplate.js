4369fc40d9e8e8a60ff044956477b4b99d79f9ff
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2021-03-11 22:27:27 +0600

    get post request boilerplate

---
+++ learn javascript/learn http request/fetch GET Requests boilerplate.js
@@ -0,0 +1,10 @@
+fetch('https://api-to-call.com/endpoint').then( response => {
+  if (response.ok){
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  return jsonResponse;
+});
---
+++ learn javascript/learn http request/fetch POST Requests boilerplate.js
@@ -0,0 +1,12 @@
+fetch('https://api-to-call.com/endpoint', {method:'POST',body:JSON.stringify({id: '200'})}).then(
+  response => {
+    if (response.ok){
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }
+, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  return jsonResponse;
+});
