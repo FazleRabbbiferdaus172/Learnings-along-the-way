cf61f755d9ed95eeee087dfcdb60e031cce74367
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2021-03-16 16:40:35 +0600

    animal fun fact done

---
+++ react/animal fun fact/app.js
@@ -0,0 +1,45 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+let title = '';
+const showBackground = false;
+title = title === '' ? "Click an animal for a fun fact" : title;
+const background = <img className="background" alt="ocean" src="/images/ocean.jpg" />;
+const images = [];
+
+for (const animal in animals) {
+    images.push(<img
+        key={animal}
+        className="animal"
+        alt={animal}
+        src={animals[animal].image}
+        ariaLabel={animal}
+        role='button'
+        onClick={displayfact}
+    />)
+}
+function displayfact(e) {
+    const selectedAnimal = e.target.alt;
+    const animalInfo = animals[selectedAnimal];
+    const optionIndex = Math.floor(Math.random()
+        * animalInfo.facts.length);
+
+    const funFact = animalInfo.facts[optionIndex];
+    document.getElementById('fact').innerHTML = funFact;
+}
+const animalFacts = (
+    <div>
+        <h1>
+            {title}
+        </h1>
+        {showBackground && background}
+        <p id='fact'>
+        </p>
+        <div className="animals">
+            {images}
+        </div>
+    </div>
+);
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
